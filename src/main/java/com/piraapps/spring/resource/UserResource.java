b73b7eb4ebeec7348f8f7399fa5ae792e0961e69
Lucas Marques <gameslucas@gmail.com>
2020-09-24 11:31:09 -0300

    Tratamento de erro

--- src/main/java/com/piraapps/spring/resource/UserResource.java
+++ src/main/java/com/piraapps/spring/resource/UserResource.java
@@ -1,14 +1,18 @@
 package com.piraapps.spring.resource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.piraapps.spring.domain.User;
+import com.piraapps.spring.dto.UserDTO;
 import com.piraapps.spring.service.UserService;
 
 @RestController
@@ -19,8 +23,17 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
+		
+		
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	@GetMapping(value="/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		UserDTO userDTO = new UserDTO(user);
+		return ResponseEntity.ok().body(userDTO);
 	}
 }
